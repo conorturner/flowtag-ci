ab3304b0b6bfbc2199b97f5de3255f8f9db42a18
Conor Turner <conorturner@live.com>
2018-05-20 18:53:17 +0100

    add branch flag

--- index.js
+++ index.js
@@ -3,10 +3,14 @@
 const program = require('commander');
 const execSync = require('child_process').execSync;
 
-program.version(require("./package").version).parse(process.argv);
+program
+	.version(require("./package").version)
+	.option('-b, --branch [branch]')
+	.parse(process.argv);
+
 
 const tags = execSync(`git tag`).toString().split("\n");
-const branch = execSync("git branch;").toString().substr(2).trim();
+const branch = program.branch || execSync("git rev-parse --abbrev-ref HEAD;").toString();
 
 const currentVersion = "v" + require(`${process.cwd()}/package.json`).version;
 const postfix = branch === "master" ? "" : `-${branch}`;
@@ -18,5 +22,5 @@ if(tags.includes(currentTag)) {
 }
 else {
 	execSync(`git tag ${currentTag}`);
-	console.log(execSync(`git push --tags`).toString());
+	console.log(`Tagged as: ${currentTag}`);
 }
